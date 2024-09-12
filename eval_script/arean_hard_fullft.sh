c04386ed62d31b55f66322b75ffaa83e871e6e02
lingjiejiang <lingjiejiang@stu.pku.edu.cn>
2024-09-11 23:32:52 -0700

    full finetune auto eval upgrade

--- eval_script/arean_hard_fullft.sh
+++ eval_script/arean_hard_fullft.sh
@@ -14,8 +14,8 @@ CHECKPOINT=${3:-$DEFAULT_CHECKPOINT}
 # 模板文件列表  
 TEMPLATE_FILES=(  
   "config/config_template/api_config_template_fullft.yaml"  
-  "config/config_template/gen_answer_config_template.yaml"  
-  "config/config_template/judge_config_template.yaml"  
+  "config/config_template/gen_answer_config_template_fullft.yaml"  
+  "config/config_template/judge_config_template_fullft.yaml"  
 )  
   
 # 生成的文件列表  
@@ -24,7 +24,9 @@ FILES=(
   "config/gen_answer_config.yaml"  
   "config/judge_config.yaml"  
 )  
-  
+SAVE_MODEL_ID="${MODEL_NAME}_${CHECKPOINT}"
+echo $SAVE_MODEL_ID
+
 # 遍历模板文件列表并生成新的文件  
 for i in "${!TEMPLATE_FILES[@]}"; do  
   TEMPLATE_FILE="${TEMPLATE_FILES[$i]}"  
@@ -34,14 +36,16 @@ for i in "${!TEMPLATE_FILES[@]}"; do
   cp "$TEMPLATE_FILE" "$OUTPUT_FILE"  
   
   # 在目标文件中进行替换  
-  sed -i "s/{{MODEL_NAME}}/${MODEL_NAME}/g" "$OUTPUT_FILE"  
+  sed -i "s/{{SAVE_MODEL_ID}}/${SAVE_MODEL_ID}/g" "$OUTPUT_FILE" 
+  sed -i "s/{{MODEL_NAME}}/${MODEL_NAME}/g" "$OUTPUT_FILE" 
   sed -i "s/{{HYPERPARAMETER}}/${HYPERPARAMETER}/g" "$OUTPUT_FILE"  
   sed -i "s/{{CHECKPOINT}}/${CHECKPOINT}/g" "$OUTPUT_FILE"
 done  
   
 # 检查文件是否存在  
-if [ -f "data/arena-hard-v0.1/model_answer/${MODEL_NAME}.jsonl" ]; then  
-  echo "文件 data/arena-hard-v0.1/model_answer/${MODEL_NAME}.jsonl 存在，跳过 vllm 服务启动和 gen_answer.py 执行。"  
+
+if [ -f "data/arena-hard-v0.1/model_answer/${SAVE_MODEL_ID}.jsonl" ]; then  
+  echo "文件 data/arena-hard-v0.1/model_answer/${SAVE_MODEL_ID}.jsonl 存在，跳过 vllm 服务启动和 gen_answer.py 执行。"  
 else  
   # 继续执行后续的指令  
   nohup vllm serve /mnt/lingjiejiang/textual_aesthetics/exp/saves/${MODEL_NAME}/${HYPERPARAMETER}/sft/${CHECKPOINT} --dtype auto --api-key token-abc123 > vllm_service.log 2>&1 &  
