978e8b338669a280bd895377e5a2c11e2f421520
lingjiejiang <lingjiejiang@stu.pku.edu.cn>
2024-09-21 06:25:42 +0000

    eval new modes

--- temp.py
+++ temp.py
@@ -1,23 +1,12 @@
-import os  
-  
-def list_directories(path, output_file):  
-    try:  
-        # 打开输出文件  
-        with open(output_file, 'w') as f:  
-            # 遍历指定目录下的所有文件和目录  
-            for item in os.listdir(path):  
-                # 构造完整路径  
-                full_path = os.path.join(path, item)  
-                # 检查是否为目录  
-                if os.path.isdir(full_path):  
-                    f.write(f'"{item}"\n')  
-        print(f"Directory names have been written to {output_file}")  
-    except Exception as e:  
-        print(f"Error: {e}")  
-  
-if __name__ == "__main__":  
-    # 指定要遍历的目录路径  
-    directory_path = "/mnt/lingjiejiang/textual_aesthetics/model_checkpoint/sft_merge_checkpoints"  
-    output_file_path = "output_file.txt"  
-      
-    list_directories(directory_path, output_file_path) 
+from openai import OpenAI
+
+# Modify OpenAI's API key and API base to use vLLM's API server.
+openai_api_key = "EMPTY"
+openai_api_base = "http://localhost:8000/v1"
+client = OpenAI(
+    api_key=openai_api_key,
+    base_url=openai_api_base,
+)
+completion = client.completions.create(model="/mnt/lingjiejiang/textual_aesthetics/exp/saves/glanchat_v2.1_8b_2048_default_template_8500_dpo/fullft/dpo/checkpoint-2756",
+                                      prompt="San Francisco is a")
+print("Completion result:", completion)
