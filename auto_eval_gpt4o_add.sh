ae58b9248328c4929bba82663cf1ba8a55ad05b2
lingjiejiang <lingjiejiang@stu.pku.edu.cn>
2024-10-03 17:07:00 +0000

    add models

--- auto_eval_gpt4o_add.sh
+++ auto_eval_gpt4o_add.sh
@@ -2,9 +2,9 @@
 set -x
 # 参数列表  
 PARAMS=(  
-
-  "ta_rejected_noneed_length_llama3.1_instruct_2048_default_template_v2_1500"
-  "ta_rejected_llama3.1_instruct_dpo_2048_default_template-1000"
+  "Llama-3-8B-Magpie-Align-SFT-v0.1"
+  # "ta_rejected_noneed_length_llama3.1_instruct_2048_default_template_v2_1500"
+  # "ta_rejected_llama3.1_instruct_dpo_2048_default_template-1000"
     # "ta_rejected_tuluv2_dpo_2048_default_template_bsz1_acc8_1000"
     # "ta_rejected_tuluv2_dpo_2048_default_template_bsz1_acc8_1500"
   # "ta_rejected_noneed_length_llama3.1_instruct_2048_default_template_v2"
