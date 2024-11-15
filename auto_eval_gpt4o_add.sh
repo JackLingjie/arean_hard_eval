a178664119ce2ffea767951ea66b6602cea5f5a3
lingjiejiang <lingjiejiang@stu.pku.edu.cn>
2024-11-15 02:51:25 +0000

    merge res

--- auto_eval_gpt4o_add.sh
+++ auto_eval_gpt4o_add.sh
@@ -2,9 +2,12 @@
 set -x
 # 参数列表  
 PARAMS=(  
-      "Mistral-7B-Instruct-v0.3_ta_rejected_dpo_2048_v2_500"
-      "Mistral-7B-Instruct-v0.3_tapo_v2_2000"
-      "Mistral-7B-Instruct-v0.3_tapo_v2_2124"
+      # "Mistral-7B-Instruct-v0.3_tapo_v1_1500"
+      "Mistral-7B-Instruct-v0.3_tapo_v2_5e7_1500"
+      # "Mistral-7B-Instruct-v0.3_tapo_v2_5e5_1500"
+      # "Mistral-7B-Instruct-v0.3_ta_rejected_dpo_2048_v2_500"
+      # "Mistral-7B-Instruct-v0.3_tapo_v2_2000"
+      # "Mistral-7B-Instruct-v0.3_tapo_v2_2124"
       # "Mistral-7B-Instruct-v0.3_ta_rejected_dpo_2048_v2"
       # "Mistral-7B-Instruct-v0.3_tapo_v2_1500"
       # "Mistral-7B-Instruct-v0.3_ta_rejected_dpo_2048_v2_1000"
